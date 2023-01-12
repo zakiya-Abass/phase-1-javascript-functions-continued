a5d78d20ab3a71e9777a6699caaba49623a75ea4
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-12 14:30:08 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+  // console.log(`This Monday, I will ${work}.`);
+}
+// mondayWork();
+// mondayWork("work from home");
+// mondayWork("do a video call");
+//scope chain makes it possible for a function to have access to all its
+//variables and all the variables of its parent functions (This includes grandparents,any functionn that hoouses it)
+
+function wrapAdjective(character) {
+  return function (compliment = "a star") {
+    return `You are ${character}${compliment}${character}!`;
+  };
+}
